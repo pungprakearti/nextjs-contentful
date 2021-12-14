9436d5449c9c6296fa87ad255ad270875f467b1b
pungprakearti <andrew.pungprakearti@gmail.com>
2021-12-14 12:19:17 -0800

    Added Contentful functionality

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# NextJS Boilerplate  
+# NextJS and Contentful
+This is a demo using NextJS as a frontend and Contentful as a headless CMS
 
 ## Run application
 `yarn dev`
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,41 @@
+import { createClient } from 'contentful'
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
 import styles from './index.module.scss'
 
-export default function Home() {
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  })
+
+  const res = await client.getEntries({
+    content_type: 'blogPost',
+  })
+
+  return {
+    props: {
+      posts: res.items,
+    }
+  }
+}
+
+export default function Home({ posts }) {
+  console.log(posts)
 
   return (
     <div className={styles.wrap}>
-      <h1>
-        NextJS Boilerplate by Andrew Pungprakearti
-      </h1>
-      <a href='mailto:andrew.pungprakearti@gmail.com'>
-        andrew.pungprakearti@gmail.com
-      </a>
+      {posts.map((post) => {
+        const content = documentToHtmlString(post.fields.body)
+        const id = post.sys.id
+
+        return (
+          <div key={id}>
+            <img src={post.fields.featuredImage.fields.file.url} />
+            <h1>{post.fields.title}</h1>
+            <div dangerouslySetInnerHTML={{__html: content}} />
+          </div>
+        )
+      })}
     </div>
   )
 }
